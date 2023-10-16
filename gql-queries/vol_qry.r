036586a02aff78f02f0f5ac4704a1918a976237d
S195009 <martin.kleppe@student.nhh.no>
2023-10-16 22:48:00 +0200

    Typo
    Typos removed

--- gql-queries/vol_qry.r
+++ gql-queries/vol_qry.r
@@ -25,9 +25,9 @@ vol_qry <- function(id, from, to) {
 
 GQL(
   vol_qry(
-    id=stations_metadata_df$id[1], 
-    from=to_iso8601(stations_metadata_df$latestData[1],-4),
-    to=to_iso8601(stations_metadata_df$latestData[1],0)
+    id=stations_metadata$id[1], 
+    from=to_iso8601(stations_metadata$latestData[1],-4),
+    to=to_iso8601(stations_metadata$latestData[1],0)
   ),
   .url = configs$vegvesen_url
 )
